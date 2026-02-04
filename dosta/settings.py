5f964e6ac9f39834d1f7e6dfe2af034d2092935e
Syed Khizar Hussain Shah <khizarsyed4294@gmail.com>
2026-02-04 18:30:22 +0500

    login page recreated

--- dosta/settings.py
+++ dosta/settings.py
@@ -122,7 +122,7 @@ LOGIN_URL = '/admin/login/'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
